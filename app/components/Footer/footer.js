0211ea4ca34b0d3db757758f567160ec34e89e0b
Ayesha <aqaisar3@uwo.ca>
2024-01-05 12:31:40 -0500

    creating footer

---
+++ app/components/Footer/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-component'
+
+
+
+/* 
+
+Icons imported from codewonders
+Github: https://github.com/adenekan41
+
+*/
+
+import {
+    IconRight, 
+    LinkedIn,
+    Github,
+    Instagram,
+    Email,
+} from '../Icons'
