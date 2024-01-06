b9d1eef125a1dcc2c80fe7db97fb88e23c36a2ab
Ayesha <aqaisar3@uwo.ca>
2024-01-06 18:45:34 -0500

    adding to timeline

--- app/components/timeline/Timeline.js
+++ app/components/timeline/Timeline.js
@@ -1,4 +1,73 @@
 import React from 'react'
-import {Chrono} from "react-chrono"
+import {Chrono} from "react-chrono";
 
-<Chrono items={experiences} mode="VERTICAL_ALTERNATING"></Chrono>
+/**
+ * Using react-chrono library for a timeline
+ * https://www.npmjs.com/package/react-chrono
+ * 
+ * Using these examples to customize the timeline:
+ * https://github.com/prabhuignoto/react-chrono?tab=readme-ov-file#theme
+ */
+
+let experiences = [{
+    title:"May 2023 - Present",
+    cardTitle:"Undergraduate Research Fellowship",
+    cardSubtitle:"Faculty of Engineering, Western University - London, Canada",
+    cardDetailedText:"Working in AI"
+},
+
+{
+    title:"Sept - Dec 2022",
+    cardTitle:"RBC Design Thinking Program Member - London, Canada",
+    cardSubtitle:"Royal Bank of Canada",
+    cardDetailedText:"Figma"
+},
+
+{
+    title:"May - Aug 2022",
+    cardTitle:"Social Media Community Manager",
+    cardSubtitle:"Northwest London Resource Center - London, Canada",
+    cardDetailedText:"Summer camp"
+},
+
+]
+
+
+const Timeline =() =>{
+    return(
+        <div className='w-5/6' >
+             <Chrono items={experiences} mode="VERTICAL_ALTERNATING" enableOutline theme={{
+                primary:"black",
+                secondary:'transparent', 
+                titleColor:'black',
+                titleColorActive:'black',
+                cardSubtitleColor:'black',
+                cardTitleColor:"purple"
+
+
+             }}
+
+             >
+                        {/* {(item, idx) => (
+                            <div
+                            className={`${
+                                idx % 2 === 0 ? "bg-primary" : "bg-secondary"
+                            } chrono-default-content`}
+                            >
+                            <div className="text-white text-lg font-semibold mb-2">
+                                {item.title}
+                            </div>
+                          
+                            </div>
+                        )} */}
+                
+             </Chrono>
+
+        </div>
+       
+
+
+    );
+};
+
+export default Timeline
