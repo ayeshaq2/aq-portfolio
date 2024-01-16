d851349f91151fb3ecf199d1922c80f6a39f70f3
Ayesha <aqaisar3@uwo.ca>
2024-01-16 13:27:00 -0500

    footer5

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -24,7 +24,7 @@ import {
 const Footer = () => {
     return (
       <>
-      <div className='flex sm:flex-wrap justify-center space-x-4 md:space-x-20 backdrop-blur-md md:py-4 sm:py-4  md:pb-5 sm:pb-5 '>
+      <div className='flex sm:flex-wrap justify-center space-x-4 md:space-x-20 backdrop-blur-md md:py-4 sm:py-4  md:pb-5 pb-5 pt-2 '>
 
       <Link className='hover:opacity-100 opacity-75' href='https://github.com/ayeshaq2'>
           <Github></Github>
